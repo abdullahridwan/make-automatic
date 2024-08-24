5990badbbcd7c5c7ead0685c13ab9b6060ded456
Abdullah Ridwan <53100755+abdullahridwan@users.noreply.github.com>
2024-08-24 17:00:41 -0400

    init

--- components/Main.js
+++ components/Main.js
@@ -1,87 +1,74 @@
+import config from '../public/config.json';
+
+
 export default function Main() {
   return (
     <section className="text-gray-600 body-font">
       <div className="max-w-7xl mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 md:ml-24 pt-6 flex flex-col md:items-start md:text-left mb-40 items-center text-center">
-          <h1 className="mb-5 sm:text-6xl text-5xl items-center Avenir xl:w-2/2 text-gray-900">
-            We are making Stunning Websites
+          <h1 className="mb-5 sm:text-6xl text-5xl items-center Avenir xl:w-2/2 text-white">
+            {config.appName}
           </h1>
-          <p className="mb-4 xl:w-3/4 text-gray-600 text-lg">
-            nine4 is a free to use website template for websites made with
-            Next.js and styled with Tailwind CSS
+          <p className="mb-4 xl:w-3/4 text-gray-300 text-xl">
+            {config.tagline}
           </p>
           <div className="flex justify-center">
             <a
-              className="inline-flex items-center px-5 py-3 mt-2 font-medium text-white transition duration-500 ease-in-out transform bg-transparent border rounded-lg bg-gray-900"
+              className="inline-flex items-center px-6 py-4 mt-2 font-medium text-white transition duration-500 ease-in-out transform bg-transparent rounded-lg bg-gray-900 rounded-full"
+              style={{ backgroundColor: config.primaryColor }}
               href="https://github.com/r1/nine4-2/"
             >
-              <span className="justify-center">Find out more</span>
+              <span className="justify-center">👉 Get the App</span>
             </a>
           </div>
+
+          <div className="flex justify-center py-5 text-gray-300">
+            <span className="justify-center">Available on the iOS Store</span>
+          </div>
+
         </div>
+
         <div className="xl:mr-44 sm:mr-0 sm:mb-28 mb-0 lg:mb-0 mr-48 md:pl-10">
           <img
             className="w-80 md:ml-1 ml-24"
-            alt="iPhone-12"
-            src="/images/iPhone-12-Mockup.png"
+            alt="iPhone-15"
+            src={config.heroimg}
           ></img>
         </div>
       </div>
-      <section className="mx-auto">
-        <div className="container px-5 mx-auto lg:px-24 ">
-          <div className="flex flex-col w-full mb-4 text-left lg:text-center">
-            <h1 className="mb-8 text-2xl Avenir font-semibold text-black">
-              Trusted by top-tier product companies
-            </h1>
-          </div>
-          <div className="grid grid-cols-2 gap-16 mb-16 text-center lg:grid-cols-4">
-            <div className="flex items-center justify-center">
-              <img
-                src="/images/Google-Logo.webp"
-                alt="Google Logo"
-                className="block object-contain h-16 greyC"
-              ></img>
-            </div>
-            <div className="flex items-center justify-center">
-              <img
-                src="/images/Shopify-Logo.svg"
-                alt="Shopify Logo"
-                className="block object-contain h-16 greyC"
-              ></img>
-            </div>
-            <div className="flex items-center justify-center">
-              <img
-                src="/images/Cloudflare-Logo.svg"
-                alt="Cloudflare Logo"
-                className="block object-contain h-16 greyC"
-              ></img>
-            </div>
-            <div className="flex items-center justify-center">
-              <img
-                src="/images/PayPal-Logo.png"
-                alt="Paypal Logo"
-                className="block object-contain h-16 greyC"
-              ></img>
-            </div>
-          </div>
-        </div>
-      </section>
+
+
+      {/* FEATURES */}
       <div className="grr max-w-7xl pt-20 mx-auto text-center">
-        <h1 className="mb-8 text-6xl Avenir font-semibold text-gray-900">
-          Less code, less effort.
+        <h1
+          style={{ color: config.primaryColor }}
+          className="mb-8 text-6xl Avenir font-semibold"
+        >
+          Features
         </h1>
-        <h1 className="mb-8 text-2xl Avenir font-semibold text-gray-600 text-center">
-          Minify your CSS with Tailwind's built in PostCSS support.
+        <h1 className="mb-8 text-xl text-gray-300 text-center px-20">
+          {config.featuresTagline}
         </h1>
-        <div className="container flex flex-col items-center justify-center mx-auto rounded-lg ">
-          <img
-            className="object-cover object-center w-3/4 mb-10 g327 border rounded-lg shadow-md"
-            alt="Placeholder Image"
-            src="./images/placeholder.png"
-          ></img>
+
+
+
+        <div className="py-10">
+          <div className="max-w-6xl mx-auto px-10">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+              {config.features.map((feature, index) => (
+                <div key={index} className="p-6 text-left">
+                  <div className="text-5xl mb-4">{feature.icon}</div>
+                  <h3 className="text-xl font-semibold mb-2 text-gray-300">{feature.name}</h3>
+                  <p className="text-gray-400">{feature.description}</p>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
-      <section className="relative">
+
+      {/* Newsletter Section */}
+      {/* <section className="relative">
         <div className="max-w-6xl mx-auto px-4 sm:px-6 text-center">
           <div className="py-24 md:py-36">
             <h1 className="mb-5 text-6xl Avenir font-semibold text-gray-900">
@@ -105,7 +92,39 @@ export default function Main() {
             </a>
           </div>
         </div>
+      </section> */}
+
+
+      {/* 2nd Call to Action */}
+      <section className="relative">
+        <div className="max-w-6xl mx-auto px-4 py-4 sm:px-6 text-center">
+          <div className="py-24 px-10 md:py-36 p-6 rounded-lg shadow-md bg-gradient-to-r from-indigo-500 from-10% via-sky-700 via-30% to-red-500 to-90%">
+            <h1 className="mb-5 text-6xl Avenir font-semibold text-white">
+              {config.appName}
+            </h1>
+
+            <div className="flex justify-center">
+              <a
+                className="inline-flex items-center px-6 py-4 mt-2 font-medium text-white transition duration-500 ease-in-out transform bg-transparent rounded-lg bg-gray-900 rounded-full"
+                style={{ backgroundColor: config.primaryColor }}
+                href="https://github.com/r1/nine4-2/"
+              >
+                <span className="justify-center">👉 Get the App</span>
+              </a>
+            </div>
+
+
+
+
+            <div className="flex justify-center py-5 text-gray-200">
+              <span className="justify-center">Available on the iOS Store</span>
+            </div>
+          </div>
+        </div>
       </section>
+
+
+
     </section>
   );
 }
